8dfb8b8839b210def7217918b8114f507beb3948
Daniel Zoba <daniel.sobe@sorben.com>
2021-07-23 17:15:08 +0200

    take some notes

--- debug-alignment/runtest.sh
+++ debug-alignment/runtest.sh
@@ -1,1 +1,7 @@
 ../bin.release/dcg ../classes/fsttools/fsttools.def
+
+
+# https://github.com/raspberrypi/linux/issues/3099
+
+# dlp_type.c: 437-458 could be rewritten w/o converting directly from unaligned memory
+
