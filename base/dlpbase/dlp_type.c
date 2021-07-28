5cde929784e8b7097ec264b2f6ddfa2df28b4506
Daniel Zoba <daniel.sobe@sorben.com>
2021-07-28 15:38:15 +0100

    work around two alignment traps when reading from buffer

--- base/dlpbase/dlp_type.c
+++ base/dlpbase/dlp_type.c
@@ -439,7 +439,7 @@ COMPLEX64 dlp_fetch(const void* lpBuffer, INT16 nTypeCode)
  /* NOTE: Size of the types could be fetched from typetable.
           The resulting code would be more consistent.*/
   if (!lpBuffer) return CMPLX(0);
-  switch (nTypeCode)
+  switch (nTypeCode) 
   {
     case T_BOOL   : { return (COMPLEX64){*(     BOOL*)lpBuffer,0.};  }
     case T_UCHAR  : { return (COMPLEX64){*(    UINT8*)lpBuffer,0.};  }
@@ -447,8 +447,22 @@ COMPLEX64 dlp_fetch(const void* lpBuffer, INT16 nTypeCode)
     case T_USHORT : { return (COMPLEX64){*(   UINT16*)lpBuffer,0.};  }
     case T_SHORT  : { return (COMPLEX64){*(    INT16*)lpBuffer,0.};  }
     case T_UINT   : { return (COMPLEX64){*(   UINT32*)lpBuffer,0.};  }
-    case T_INT    : { return (COMPLEX64){*(    INT32*)lpBuffer,0.};  }
-    case T_ULONG  : { return (COMPLEX64){*(   UINT64*)lpBuffer,0.};  }
+    case T_INT    : 
+    	{ 
+    		/* return (COMPLEX64){*(    INT32*)lpBuffer,0.}; */  
+    		COMPLEX64 tmp; 
+    		dlp_memmove(&tmp.x, lpBuffer, sizeof(INT32*));
+    		tmp.y = 0.0f;  
+    		return tmp;
+    	}
+    case T_ULONG  : 
+    	{ 
+    		/* return (COMPLEX64){*(   UINT64*)lpBuffer,0.}; */ 
+    		COMPLEX64 tmp; 
+    		dlp_memmove(&tmp.x, lpBuffer, sizeof(UINT64));
+    		tmp.y = 0.0f;  
+    		return tmp;
+    	}
     case T_LONG   : { return (COMPLEX64){*(    INT64*)lpBuffer,0.};  }
     case T_FLOAT  : { return (COMPLEX64){*(  FLOAT32*)lpBuffer,0.};  }
     case T_DOUBLE : { return (COMPLEX64){*(  FLOAT64*)lpBuffer,0.};  }
